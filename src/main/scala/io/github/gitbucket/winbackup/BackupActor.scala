553e2f3f45bd9a7f6bae9b86b30f57d7447c8406
jyuch <jyuch@users.noreply.github.com>
2018-01-15 21:18:43 +0900

    Copy database data

--- src/main/scala/io/github/gitbucket/winbackup/BackupActor.scala
+++ src/main/scala/io/github/gitbucket/winbackup/BackupActor.scala
@@ -1,8 +1,15 @@
 package io.github.gitbucket.winbackup
 
+import java.io.File
+
 import akka.actor.{Actor, Props}
 import akka.event.Logging
+import gitbucket.core.model.Profile.profile.blockingApi._
+import gitbucket.core.servlet.Database
+import gitbucket.core.util.Directory
+import gitbucket.core.util.JDBCUtil.RichConnection
 import io.github.gitbucket.winbackup.BackupActor.DoBackup
+import org.apache.commons.io.FileUtils
 
 class BackupActor extends Actor {
 
@@ -11,6 +18,15 @@ class BackupActor extends Actor {
   override def receive: Receive = {
     case _: DoBackup => {
       logger.info("Do backup")
+
+      val backupDest = new File(Directory.GitBucketHome, "backup-" + System.currentTimeMillis())
+
+      Database() withTransaction { session =>
+        val allTables = session.conn.allTableNames()
+        val sqlFile = session.conn.exportAsSQL(allTables)
+        val sqlBackup = new File(backupDest, "gitbucket.sql")
+        FileUtils.copyFile(sqlFile, sqlBackup)
+      }
     }
   }
 
