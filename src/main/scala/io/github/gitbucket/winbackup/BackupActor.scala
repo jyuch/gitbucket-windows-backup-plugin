716411e4d65e06292e06bc8e89a78399c83d88bc
jyuch <jyuch@users.noreply.github.com>
2018-01-15 22:01:18 +0900

    Add repositories copy

--- src/main/scala/io/github/gitbucket/winbackup/BackupActor.scala
+++ src/main/scala/io/github/gitbucket/winbackup/BackupActor.scala
@@ -4,14 +4,16 @@ import java.io.File
 
 import akka.actor.{Actor, Props}
 import akka.event.Logging
+import com.github.zafarkhaja.semver.{Version => Semver}
 import gitbucket.core.model.Profile.profile.blockingApi._
+import gitbucket.core.service.{AccountService, RepositoryService}
 import gitbucket.core.servlet.Database
-import gitbucket.core.util.Directory
 import gitbucket.core.util.JDBCUtil.RichConnection
+import gitbucket.core.util.{Directory, JGitUtil}
 import io.github.gitbucket.winbackup.BackupActor.DoBackup
 import org.apache.commons.io.FileUtils
 
-class BackupActor extends Actor {
+class BackupActor extends Actor with AccountService with RepositoryService {
 
   private val logger = Logging(context.system, this)
 
@@ -21,11 +23,20 @@ class BackupActor extends Actor {
 
       val backupDest = new File(Directory.GitBucketHome, "backup-" + System.currentTimeMillis())
 
-      Database() withTransaction { session =>
+      Database() withTransaction { implicit session =>
         val allTables = session.conn.allTableNames()
         val sqlFile = session.conn.exportAsSQL(allTables)
         val sqlBackup = new File(backupDest, "gitbucket.sql")
         FileUtils.copyFile(sqlFile, sqlBackup)
+
+        for (
+          user <- getAllUsers();
+          (userName, repoName) <- getAllRepositories(user.userName)
+        ) {
+          val src = Directory.getRepositoryDir(userName, repoName)
+          val dest = new File(new File(backupDest, userName), repoName)
+          JGitUtil.cloneRepository(src, dest)
+        }
       }
     }
   }
